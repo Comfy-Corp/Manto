082090c92fab8024e79ebedb8530a331723eea0a
Guus Beckett <guus@reupload.nl>
2015-11-30 15:02:26 +0100

    Add interface and text to speech

--- Manto/MainPage.xaml.cs
+++ Manto/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Media.SpeechSynthesis;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -26,5 +27,28 @@ namespace Manto
         {
             this.InitializeComponent();
         }
+
+        private async void Fortune_Text_Click(object sender, TappedRoutedEventArgs e)
+        {
+            MediaElement media = new MediaElement();
+            string text = textBlockFortune.Text;
+            SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+            SpeechSynthesisStream stream = await synthesizer.SynthesizeTextToStreamAsync(text + ", God bless America");
+            media.SetSource(stream, stream.ContentType);
+            media.IsLooping = true;
+            media.Play();
+        }
+
+        private void randomNewFortune(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+
+        public string randomFortune()
+        {
+            return "Placeholder";
+        }
     }
+
+
 }
